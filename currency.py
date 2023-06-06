0d249392a18cc4af8118778883ce3b40129c4cfe
leesper <pascal7718@gmail.com>
2023-06-06 21:28:48 +0800

    feat: common equaals

--- currency.py
+++ currency.py
@@ -1,15 +1,17 @@
-class Dollar:
-    def __init__(self, amount):
-        self.__amount = amount
-    def times(self, multiplier):
-        return Dollar(self.__amount * multiplier)
+class Money:
+    def __init__(self, amount=0):
+        self._amount = amount
     def __eq__(self, other):
-        return self.__amount == other.__amount
+        return self._amount == other._amount
+
+class Dollar(Money):
+    def __init__(self, amount):
+        super().__init__(amount)
+    def times(self, multiplier):
+        return Dollar(self._amount * multiplier)
     
-class Franc:
+class Franc(Money):
     def __init__(self, amount):
-        self.__amount = amount
+        super().__init__(amount)
     def times(self, multiplier):
-        return Franc(self.__amount * multiplier)
-    def __eq__(self, other):
-        return self.__amount == other.__amount
+        return Franc(self._amount * multiplier)
--- currency_test.py
+++ currency_test.py
@@ -12,15 +12,21 @@ class TestCurrency(unittest.TestCase):
     # TODO: hashCode
     # TODO: equal null
     # TODO: equal object
+    # TODO: Dollar/Franc duplication
+    # TODO: Common times
+    # TODO: Compare Francs with Dollars
     def testMultiplication(self):
         five = Dollar(5)
         self.assertEqual(Dollar(10), five.times(2))
         self.assertEqual(Dollar(15), five.times(3))
 
     # equals
+    # Common equals
     def testEquality(self):
         self.assertTrue(Dollar(5) == Dollar(5))
         self.assertFalse(Dollar(5) == Dollar(6))
+        self.assertTrue(Franc(5) == Franc(5))
+        self.assertFalse(Franc(5) == Franc(6))
 
     # 5CHF * 2 = 10CHF
     def testFrancMultiplication(self):
